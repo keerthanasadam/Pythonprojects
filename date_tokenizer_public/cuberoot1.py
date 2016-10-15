e2cba908906cb213ecf7eab40ddc37fdd3801c88
keerthanasadam <keerthana.sadam@gmail.com>
2016-10-15 17:58:18 -0500

    Added date tokenizer program files

---
+++ date_tokenizer_public/cuberoot1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 10 20:53:42 2016
+
+@author: keert
+"""
+
+x = 25
+epsilon = 0.01
+step = 0.1
+guess = 0.0
+
+while guess <= x:
+    if abs(guess**2 -x) >= epsilon:
+        guess += step
+
+if abs(guess**2 - x) >= epsilon:
+    print('failed')
+else:
+    print('succeeded: ' + str(guess))
